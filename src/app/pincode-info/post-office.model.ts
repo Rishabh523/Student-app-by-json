d082afe31e352543fd5b71ef8b02f06705f1c711
Rishabh523 <parishabh8787@gmail.com>
2024-08-02 11:44:48 +0530

    p

---
+++ src/app/pincode-info/post-office.model.ts
@@ -0,0 +1,18 @@
+export interface POffice {
+    Message: string;
+    Status: string;
+    PostOffice: PostOfficeDetailModel[];
+  }
+  
+  export interface PostOfficeDetailModel {
+    Name: string;
+    Description: string;
+    BranchType: string;
+    DeliveryStatus: string;
+    Circle: string;
+    District: string;
+    Division: string;
+    Region: string;
+    State: string;
+    Country: string;
+  }
