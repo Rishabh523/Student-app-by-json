ec00c6a2d070b839d0bb6dd9202264ebcd25c13c
Rishabh523 <parishabh8787@gmail.com>
2024-08-02 11:29:40 +0530

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,54 +1,34 @@
-import { Component, inject } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HousingLocationComponent } from "../housing-location/housing-location.component";
-import { HousingLocation } from "../housinglocation";
-import { HousingService } from "../housing.service";
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { ICity } from './city.model';
 
 @Component({
-  selector: "app-home",
+  selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter />
-        <button
-          class="primary"
-          type="button"
-          (click)="filterResults(filter.value)"
-        >
-          Search
-        </button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of filteredLocationList"
-        [housingLocation]="housingLocation"
-      ></app-housing-location>
-    </section>
-  `,
-  styleUrls: ["./home.component.css"],
+  imports: [NgFor, NgIf, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = [];
-  housingService: HousingService = inject(HousingService);
-  filteredLocationList: HousingLocation[] = [];
-  constructor() {
-    this.housingService
-      .getAllHousingLocations()
-      .then((housingLocationList: HousingLocation[]) => {
-        this.housingLocationList = housingLocationList;
-        this.filteredLocationList = housingLocationList;
-      });
-  }
-  filterResults(text: string) {
-    if (!text) {
-      this.filteredLocationList = this.housingLocationList;
-    }
-    this.filteredLocationList = this.housingLocationList.filter(
-      (housingLocation) =>
-        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
-    );
-  }
+  cities: ICity[] = [
+    {
+      id: 20,
+      name: 'New York',
+      picture: 'assestsimage\newy.jpeg',
+    },
+    {
+      name: 'New Jersy',
+      id: 21,
+      picture: 'assestsimage\newy1.jpeg',
+    },
+    {
+      name: 'Albama',
+      id: 22,
+      picture: 'assestsimage\newy2.jpeg',
+    },
+    {
+      name: 'Taxes',
+      id: 23,
+    },
+  ];
 }
